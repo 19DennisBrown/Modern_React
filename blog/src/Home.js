6f81dd1bb48d3f70d969eeff7aa016150644aa0d
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2024-09-21 08:59:13 +0300

    useHook

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,17 +1,21 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import BlogList from './Blog_list';
+import useFetch from './useFetch';
 
 const  Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const {data:blogs, isLoading, error} = useFetch("http://localhost:4000/blogs")
+
+ 
   return ( 
     <main className="home">
-      <BlogList blogs={blogs.filter((blog)=>blog.author === 'mario')} />
+      { error && <p> { error } </p>}
+      { isLoading && <div>loading...</div>}
+      {blogs && <BlogList
+      //  blogs={blogs.filter((blog)=>blog.author === 'mario')} 
+       blogs = {blogs}
+      />}
     </main>
    );
 }
